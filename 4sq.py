f8c70eb398e5c1577f40f66599caa4b2c2e2c5b2
f1vlad <kulchitski@gmail.com>
2010-07-16 13:51:24 -0400

    add 4sq.py

---
+++ 4sq.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python
+
+import urllib
+user = "f1vlad" # specify your foursquare username
+output_path = "/var/www/fs.f1vlad.com/etc/%s.4sq.xml" % user # specify output file where location would be written
+url = "http://foursquare.com/search?q=%s&x=0&y=0" % user # nothing to touch here
+
+try:
+    page = urllib.urlopen(url)
+    loc = page.read()
+    start=loc.index("div class=\"address\"")+20
+    offset = loc[start+20:start+400].index("</div>")+1
+except IOerror:
+    loc = 'Unknown'
+
+loc=loc[start:start+offset]
+f = open(output_path,'w')
+f.write(loc)
+# print loc[start:start+offset]
+
