5cd2c2c92b79b3dca26a4d2c05d2c015ce7ab8f2
f1vlad <kulchitski@gmail.com>
2012-03-04 00:30:09 -0500

    Misc updates

--- 4sq.py
+++ 4sq.py
@@ -5,19 +5,28 @@ user = "f1vlad" # specify your foursquare username
 output_path = "/var/www/fs.f1vlad.com/etc/%s.4sq.rss" % user # specify output file where location would be written
 url = "http://foursquare.com/search?q=%s&x=0&y=0" % user # nothing to touch here
 
+
+
+#print 'test'
+#page = urllib.urlopen('http://foursquare.com/search?q=f1vlad&x=0&y=0')
+#loc = page.read()
+#print loc
+
+
+
 try:
     page = urllib.urlopen(url)
     loc = page.read()
-    start=loc.index("div class=\"address\"")+20
+    start = loc.index("div class=\"address\"")+20
     offset = loc[start:start+400].index("</div>")
     loc=loc[start:start+offset]
-except IOerror:
-    loc = 'Unknown'
+except:
+    loc = 'Off the grid'
 
-rss_file = '<rss version="2.0"><channel><title>%s last spotted location</title><link>http://f1vlad.com</link><description></description><language>en-us</language><lastBuildDate></lastBuildDate><item><title>%s</title><link>http://</link><description></description><pubDate></pubDate></item></channel></rss>' % (user,loc)
+rss_file = '<rss version="2.0"><channel><title>%s last spotted location</title><link>http://f1vlad.com</link><description></description><language>en-us</language><lastBuildDate></lastBuildDate><item><title>%s</title><link>http://</link><description></description><pp
+ubDate></pubDate></item></channel></rss>' % (user,loc)
 
 
 f = open(output_path,'w')
 f.write(rss_file)
 # print loc[start:start+offset]
-
