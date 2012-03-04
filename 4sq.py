affb901d8990f713216e5e229e055833f9648ef5
f1vlad <kulchitski@gmail.com>
2012-03-04 01:31:17 -0500

    Clean up

--- 4sq.py
+++ 4sq.py
@@ -6,11 +6,11 @@ output_path = "/var/www/fs.f1vlad.com/etc/%s.4sq.rss" % user # specify output fi
 url = "http://foursquare.com/search?q=%s&x=0&y=0" % user # nothing to touch here
 alt_url = "http://pipes.yahoo.com/pipes/pipe.run?_id=fd4701baf3d6eed65eb27fca4801ab6a&_render=json"
 
-print 'test'
+#print 'test'
 #page = urllib.urlopen('http://foursquare.com/search?q=f1vlad&x=0&y=0')
-page = urllib.urlopen(alt_url)
-loc = page.read()
-print loc
+#page = urllib.urlopen(alt_url)
+#loc = page.read()
+#print loc
 
 try:
     page = urllib.urlopen(alt_url)
@@ -21,10 +21,10 @@ try:
 except:
     loc = 'Off the grid'
 
-#rss_file = '<rss version="2.0"><channel><title>%s last spotted location</title><link>http://f1vlad.com</link><description></description><language>en-us</language><lastBuildDate></lastBuildDate><item><title>%s</title><link>http://</link><description></description><<
-pubDate></pubDate></item></channel></rss>' % (user,loc)
+rss_file = '<rss version="2.0"><channel><title>%s last spotted location</title><link>http://f1vlad.com</link><description></description><language>en-us</language><lastBuildDate></lastBuildDate><item><title>%s</title><link>http://</link><description></description><pp
+ubDate></pubDate></item></channel></rss>' % (user,loc)
 
-rss_file = loc
+#rss_file = loc
 
 f = open(output_path,'w')
 f.write(rss_file)
